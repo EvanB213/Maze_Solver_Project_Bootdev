ef4a3cd97e31b5f8b573fc7a0c8fdc0fc0b4e725
EvanB213 <evanb2132@gmail.com>
2024-07-25 12:48:29 -0400

    Working maze construction, wall breaks, unit testing

--- maze.py
+++ maze.py
@@ -1,10 +1,11 @@
 from cell import Cell
 from graphics import *
 import time
+import random
 
 class Maze:
 
-    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None,) -> None:
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None, seed=None) -> None:
         self.x1 = x1
         self.y1 = y1
         self.num_rows = num_rows
@@ -13,7 +14,12 @@ class Maze:
         self.cell_size_y = cell_size_y
         self.win = win
         self._cells = []
+        if seed:
+            self.seed = random.seed(seed)
         self._create_cells()
+        self._break_entrance_and_exit()
+        self._break_walls_r(0, 0)
+        self._reset_cells_visited()
     
     def _create_cells(self):
         for i in range(self.num_cols):
@@ -42,3 +48,52 @@ class Maze:
             return
         self.win.redraw()
         time.sleep(0.05)
+
+    def _break_entrance_and_exit(self):
+        self._cells[0][0].has_top_wall = False
+        self._draw_cell(0, 0)
+
+        self._cells[self.num_cols-1][self.num_rows-1].has_bottom_wall = False
+        self._draw_cell(self.num_cols-1, self.num_rows-1)
+
+    def _break_walls_r(self, i, j):
+        self._cells[i][j].visited = True
+
+        while True:
+            to_visit = []
+            if i > 0 and not self._cells[i-1][j].visited:
+                to_visit.append((i-1, j))
+            if i < self.num_cols-1 and not self._cells[i+1][j].visited:
+                to_visit.append((i+1, j))
+            if j > 0 and not self._cells[i][j-1].visited:
+                to_visit.append((i, j-1))
+            if j < self.num_rows-1 and not self._cells[i][j+1].visited:
+                to_visit.append((i, j+1))
+
+            if len(to_visit) == 0:
+                self._draw_cell(i, j)
+                return
+
+            rand_dir = random.randrange(len(to_visit))
+            direction = to_visit[rand_dir]
+
+            if direction[0] == i-1:
+                self._cells[i][j].has_left_wall = False
+                self._cells[i-1][j].has_right_wall = False
+            if direction[0] == i+1:
+                self._cells[i][j].has_right_wall = False
+                self._cells[i+1][j].has_left_wall = False
+            if direction[1] == j-1:
+                self._cells[i][j].has_top_wall = False
+                self._cells[i][j-1].has_bottom_wall = False
+            if direction[1] == j+1:
+                self._cells[i][j].has_bottom_wall = False
+                self._cells[i][j+1].has_top_wall = False
+            
+            self._break_walls_r(direction[0], direction[1])
+
+    def _reset_cells_visited(self):
+        for column in self._cells:
+            for cell in column:
+                cell.visited = False
+        
